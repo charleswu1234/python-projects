ce7452ac8b6b45020b8cb4a1583b1519de7cff75
charleswu1234 <99754178+charleswu1234@users.noreply.github.com>
2022-06-24 13:07:55 -0400

    First commit v3

---
+++ scraping-project/politics_scraper.py
@@ -0,0 +1,45 @@
+## Importing data
+import numpy as np
+import scrapy
+import re
+import requests
+from bs4 import BeautifulSoup
+
+## Information needed
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+url = 'https://reddit.com/r/politics/'
+source = requests.get(url, headers = headers)
+soup = BeautifulSoup(source.text, 'html.parser')
+
+## Filtering through data
+div = soup.find_all('div', class_ = 'y8HYJ-y_lTUHkQIc1mdCq _2INHSNB8V5eaWp4P0rY_mE')
+titles = []
+for d in div:
+    titles.append(d.a.text)
+
+## Dictionary of number of times word is repeated in titles
+dic = {}
+for title in titles:
+    words_symbols = title.split(' ')
+    print(words_symbols)
+    for word_symbol in words_symbols:
+        word = re.sub("[^a-zA-Z]+", "", word_symbol)
+        word = word.lower()
+        word_list = [word if word != '']
+        if word in dic.keys() & word != '':
+            dic[word] += 1
+        elif word not in dic.keys() & word != '':
+            dic[word] = 1
+
+## Finding most used word
+most = []
+keys = list(dic.keys())
+values = list(dic.values())
+values_unique = np.unique(np.array(values))
+values_unique_sorted = values_unique[::-1]
+print(values_unique_sorted)
+for i in values_unique_sorted:
+    keys = [k for k, v in dic.items() if v == i]
+    for key in keys:
+        most.append(f'{key} {i}')
+print(most)
